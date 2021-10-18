daa6db4c3e3381093c91c75cfcdaf1478e364e6b
SanchezCruzPatricia <psancru2605@g.educaand.es>
2021-10-18 23:03:01 +0200

    first commit

--- src/collection.js
+++ src/collection.js
@@ -1,93 +1,13 @@
-db.collection.insertOne(
-    { nombre: "Floyd Mayweather Jr", edad: "44", estatura "173cm", peso: "68kg", division: ["peso welter", "peso ligero", "peso pluma", "peso superpluma", "peso superwelter", "peso superligero"], campeon: [ "CBM", "FIB"], activo: "no", victorias: ["50", [ ko: "27",pordecision: "23" ] ], derrotas: ["0", [derrotasko: "0", derrotaspordecisión: "0" ] ], empate: "0" }, 
-    { nombre: "Manny Pacquio", edad: "42", estatura "166cm", peso: "67kg", division: ["mosca", "supergallo", "pluma", "superpluma", "ligero", "superligero", "welter", "superwelter" ], campeon: [ "CMB", "PSA", "ESPN" ], activo: "no", victorias: ["62", [ko: "39", pordecision: "23" ] ], derrotas: [ "8", [ ko: "3", pordecisión: "5" ] ], empate: "2" }, 
-    { nombre: "", edad: "", estatura "54", peso: "", division: ["cotton"], campeon: [ "" ], activo: "", victorias: ["", [ko: "", pordecision: "" ] ], derrotas: [ "", [ ko: "", pordecisión: "" ] ], empate: "" }, 
-    { nombre: "", edad: "", estatura "54", peso: "", division: ["cotton"], campeon: [ "" ], activo: "", victorias: ["", [ko: "", pordecision: "" ] ], derrotas: [ "", [ ko: "", pordecisión: "" ] ], empate: "" }, 
-    { nombre: "", edad: "", estatura "54", peso: "", division: ["cotton"], campeon: [ "" ], activo: "", victorias: ["", [ko: "", pordecision: "" ] ], derrotas: [ "", [ ko: "", pordecisión: "" ] ], empate: "" }, 
-    { nombre: "", edad: "", estatura "54", peso: "", division: ["cotton"], campeon: [ "" ], activo: "", victorias: ["", [ko: "", pordecision: "" ] ], derrotas: [ "", [ ko: "", pordecisión: "" ] ], empate: "" }, 
-    { nombre: "", edad: "", estatura "54", peso: "", division: ["cotton"], campeon: [ "" ], activo: "", victorias: ["", [ko: "", pordecision: "" ] ], derrotas: [ "", [ ko: "", pordecisión: "" ] ], empate: "" }, 
-    { nombre: "", edad: "", estatura "54", peso: "", division: ["cotton"], campeon: [ "" ], activo: "", victorias: ["", [ko: "", pordecision: "" ] ], derrotas: [ "", [ ko: "", pordecisión: "" ] ], empate: "" }, 
-    { nombre: "", edad: "", estatura "54", peso: "", division: ["cotton"], campeon: [ "" ], activo: "", victorias: ["", [ko: "", pordecision: "" ] ], derrotas: [ "", [ ko: "", pordecisión: "" ] ], empate: "" }, 
-    { nombre: "", edad: "", estatura "54", peso: "", division: ["cotton"], campeon: [ "" ], activo: "", victorias: ["", [ko: "", pordecision: "" ] ], derrotas: [ "", [ ko: "", pordecisión: "" ] ], empate: "" }, 
-    { nombre: "", edad: "", estatura "54", peso: "", division: ["cotton"], campeon: [ "" ], activo: "", victorias: ["", [ko: "", pordecision: "" ] ], derrotas: [ "", [ ko: "", pordecisión: "" ] ], empate: "" }, 
-    { nombre: "", edad: "", estatura "54", peso: "", division: ["cotton"], campeon: [ "" ], activo: "", victorias: ["", [ko: "", pordecision: "" ] ], derrotas: [ "", [ ko: "", pordecisión: "" ] ], empate: "" }, 
-    { nombre: "", edad: "", estatura "54", peso: "", division: ["cotton"], campeon: [ "" ], activo: "", victorias: ["", [ko: "", pordecision: "" ] ], derrotas: [ "", [ ko: "", pordecisión: "" ] ], empate: "" }, 
-    { nombre: "", edad: "", estatura "54", peso: "", division: ["cotton"], campeon: [ "" ], activo: "", victorias: ["", [ko: "", pordecision: "" ] ], derrotas: [ "", [ ko: "", pordecisión: "" ] ], empate: "" }, 
-    { nombre: "", edad: "", estatura "54", peso: "", division: ["cotton"], campeon: [ "" ], activo: "", victorias: ["", [ko: "", pordecision: "" ] ], derrotas: [ "", [ ko: "", pordecisión: "" ] ], empate: "" }, 
-    { nombre: "", edad: "", estatura "54", peso: "", division: ["cotton"], campeon: [ "" ], activo: "", victorias: ["", [ko: "", pordecision: "" ] ], derrotas: [ "", [ ko: "", pordecisión: "" ] ], empate: "" }, 
-    { nombre: "", edad: "", estatura "54", peso: "", division: ["cotton"], campeon: [ "" ], activo: "", victorias: ["", [ko: "", pordecision: "" ] ], derrotas: [ "", [ ko: "", pordecisión: "" ] ], empate: "" }, 
-    { nombre: "", edad: "", estatura "54", peso: "", division: ["cotton"], campeon: [ "" ], activo: "", victorias: ["", [ko: "", pordecision: "" ] ], derrotas: [ "", [ ko: "", pordecisión: "" ] ], empate: "" }, 
-    { nombre: "", edad: "", estatura "54", peso: "", division: ["cotton"], campeon: [ "" ], activo: "", victorias: ["", [ko: "", pordecision: "" ] ], derrotas: [ "", [ ko: "", pordecisión: "" ] ], empate: "" }, 
-    { nombre: "", edad: "", estatura "54", peso: "", division: ["cotton"], campeon: [ "" ], activo: "", victorias: ["", [ko: "", pordecision: "" ] ], derrotas: [ "", [ ko: "", pordecisión: "" ] ], empate: "" }, 
-    { nombre: "", edad: "", estatura "54", peso: "", division: ["cotton"], campeon: [ "" ], activo: "", victorias: ["", [ko: "", pordecision: "" ] ], derrotas: [ "", [ ko: "", pordecisión: "" ] ], empate: "" }, 
-    { nombre: "", edad: "", estatura "54", peso: "", division: ["cotton"], campeon: [ "" ], activo: "", victorias: ["", [ko: "", pordecision: "" ] ], derrotas: [ "", [ ko: "", pordecisión: "" ] ], empate: "" }, 
-    { nombre: "", edad: "", estatura "54", peso: "", division: ["cotton"], campeon: [ "" ], activo: "", victorias: ["", [ko: "", pordecision: "" ] ], derrotas: [ "", [ ko: "", pordecisión: "" ] ], empate: "" }, 
-    { nombre: "", edad: "", estatura "54", peso: "", division: ["cotton"], campeon: [ "" ], activo: "", victorias: ["", [ko: "", pordecision: "" ] ], derrotas: [ "", [ ko: "", pordecisión: "" ] ], empate: "" }, 
-    { nombre: "", edad: "", estatura "54", peso: "", division: ["cotton"], campeon: [ "" ], activo: "", victorias: ["", [ko: "", pordecision: "" ] ], derrotas: [ "", [ ko: "", pordecisión: "" ] ], empate: "" }, 
-    { nombre: "", edad: "", estatura "54", peso: "", division: ["cotton"], campeon: [ "" ], activo: "", victorias: ["", [ko: "", pordecision: "" ] ], derrotas: [ "", [ ko: "", pordecisión: "" ] ], empate: "" }, 
-    { nombre: "", edad: "", estatura "54", peso: "", division: ["cotton"], campeon: [ "" ], activo: "", victorias: ["", [ko: "", pordecision: "" ] ], derrotas: [ "", [ ko: "", pordecisión: "" ] ], empate: "" }, 
-    { nombre: "", edad: "", estatura "54", peso: "", division: ["cotton"], campeon: [ "" ], activo: "", victorias: ["", [ko: "", pordecision: "" ] ], derrotas: [ "", [ ko: "", pordecisión: "" ] ], empate: "" }, 
-    { nombre: "", edad: "", estatura "54", peso: "", division: ["cotton"], campeon: [ "" ], activo: "", victorias: ["", [ko: "", pordecision: "" ] ], derrotas: [ "", [ ko: "", pordecisión: "" ] ], empate: "" }, 
-    { nombre: "", edad: "", estatura "54", peso: "", division: ["cotton"], campeon: [ "" ], activo: "", victorias: ["", [ko: "", pordecision: "" ] ], derrotas: [ "", [ ko: "", pordecisión: "" ] ], empate: "" }, 
-    { nombre: "", edad: "", estatura "54", peso: "", division: ["cotton"], campeon: [ "" ], activo: "", victorias: ["", [ko: "", pordecision: "" ] ], derrotas: [ "", [ ko: "", pordecisión: "" ] ], empate: "" }, 
-    { nombre: "", edad: "", estatura "54", peso: "", division: ["cotton"], campeon: [ "" ], activo: "", victorias: ["", [ko: "", pordecision: "" ] ], derrotas: [ "", [ ko: "", pordecisión: "" ] ], empate: "" }, 
-    { nombre: "", edad: "", estatura "54", peso: "", division: ["cotton"], campeon: [ "" ], activo: "", victorias: ["", [ko: "", pordecision: "" ] ], derrotas: [ "", [ ko: "", pordecisión: "" ] ], empate: "" }, 
-    { nombre: "", edad: "", estatura "54", peso: "", division: ["cotton"], campeon: [ "" ], activo: "", victorias: ["", [ko: "", pordecision: "" ] ], derrotas: [ "", [ ko: "", pordecisión: "" ] ], empate: "" }, 
-    { nombre: "", edad: "", estatura "54", peso: "", division: ["cotton"], campeon: [ "" ], activo: "", victorias: ["", [ko: "", pordecision: "" ] ], derrotas: [ "", [ ko: "", pordecisión: "" ] ], empate: "" }, 
-    { nombre: "", edad: "", estatura "54", peso: "", division: ["cotton"], campeon: [ "" ], activo: "", victorias: ["", [ko: "", pordecision: "" ] ], derrotas: [ "", [ ko: "", pordecisión: "" ] ], empate: "" }, 
-    { nombre: "", edad: "", estatura "54", peso: "", division: ["cotton"], campeon: [ "" ], activo: "", victorias: ["", [ko: "", pordecision: "" ] ], derrotas: [ "", [ ko: "", pordecisión: "" ] ], empate: "" }, 
-    { nombre: "", edad: "", estatura "54", peso: "", division: ["cotton"], campeon: [ "" ], activo: "", victorias: ["", [ko: "", pordecision: "" ] ], derrotas: [ "", [ ko: "", pordecisión: "" ] ], empate: "" }, 
-    { nombre: "", edad: "", estatura "54", peso: "", division: ["cotton"], campeon: [ "" ], activo: "", victorias: ["", [ko: "", pordecision: "" ] ], derrotas: [ "", [ ko: "", pordecisión: "" ] ], empate: "" }, 
-    { nombre: "", edad: "", estatura "54", peso: "", division: ["cotton"], campeon: [ "" ], activo: "", victorias: ["", [ko: "", pordecision: "" ] ], derrotas: [ "", [ ko: "", pordecisión: "" ] ], empate: "" }, 
-    { nombre: "", edad: "", estatura "54", peso: "", division: ["cotton"], campeon: [ "" ], activo: "", victorias: ["", [ko: "", pordecision: "" ] ], derrotas: [ "", [ ko: "", pordecisión: "" ] ], empate: "" }, 
-    { nombre: "", edad: "", estatura "54", peso: "", division: ["cotton"], campeon: [ "" ], activo: "", victorias: ["", [ko: "", pordecision: "" ] ], derrotas: [ "", [ ko: "", pordecisión: "" ] ], empate: "" }, 
-    { nombre: "", edad: "", estatura "54", peso: "", division: ["cotton"], campeon: [ "" ], activo: "", victorias: ["", [ko: "", pordecision: "" ] ], derrotas: [ "", [ ko: "", pordecisión: "" ] ], empate: "" }, 
-    { nombre: "", edad: "", estatura "54", peso: "", division: ["cotton"], campeon: [ "" ], activo: "", victorias: ["", [ko: "", pordecision: "" ] ], derrotas: [ "", [ ko: "", pordecisión: "" ] ], empate: "" }, 
-    { nombre: "", edad: "", estatura "54", peso: "", division: ["cotton"], campeon: [ "" ], activo: "", victorias: ["", [ko: "", pordecision: "" ] ], derrotas: [ "", [ ko: "", pordecisión: "" ] ], empate: "" }, 
-    { nombre: "", edad: "", estatura "54", peso: "", division: ["cotton"], campeon: [ "" ], activo: "", victorias: ["", [ko: "", pordecision: "" ] ], derrotas: [ "", [ ko: "", pordecisión: "" ] ], empate: "" }, 
-    { nombre: "", edad: "", estatura "54", peso: "", division: ["cotton"], campeon: [ "" ], activo: "", victorias: ["", [ko: "", pordecision: "" ] ], derrotas: [ "", [ ko: "", pordecisión: "" ] ], empate: "" }, 
-    { nombre: "", edad: "", estatura "54", peso: "", division: ["cotton"], campeon: [ "" ], activo: "", victorias: ["", [ko: "", pordecision: "" ] ], derrotas: [ "", [ ko: "", pordecisión: "" ] ], empate: "" }, 
-    { nombre: "", edad: "", estatura "54", peso: "", division: ["cotton"], campeon: [ "" ], activo: "", victorias: ["", [ko: "", pordecision: "" ] ], derrotas: [ "", [ ko: "", pordecisión: "" ] ], empate: "" }, 
-    )
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
-    ) 
+db.collection.insert( [
+    { nombre: "Floyd Mayweather Jr", edad: 44, estatura: "173cm", peso: "68kg", division: ["peso welter", "peso ligero", "peso pluma", "peso superpluma", "peso superwelter", "peso superligero"], campeon: [ "CBM", "FIB"], activo: "no", combates: 50 },
+    { nombre: "Manny Pacquio", edad: 42, estatura: "166cm", peso: "67kg", division: ["mosca", "supergallo", "pluma", "superpluma", "ligero", "superligero", "welter", "superwelter" ], campeon: [ "CMB", "PSA", "ESPN" ], activo: "no", combates: 71 },
+    { nombre: "Canelo", edad: 31, estatura: "173cm", peso: "76kg", division: ["peso mediano", "peso supermediano", "peso welter", "peso mediopesado", "peso superwelter", "peso superligero"], campeon: [ "AMB", "CMB", "FIB" ], activo: "si", combates: 59 },
+    { nombre: "Muhammad Ali", edad: 74, estatura: "191cm", peso: "75kg", division: ["pesos pesado"], campeon: [ "campeon indiscutible" ], activo: "no", combate: 61 },
+    { nombre: "Tyson Fury", edad: 33, estatura: "209cm", peso: "126kg", division: ["pesos pesado"], campeon: [ "WBO" ], activo: "si", combate: 32 },
+    { nombre: "Mike Tyson", edad: 55, estatura: "178cm", peso: "100kg", division: ["pesos pesado"], campeon: [ "WBO" ], activo: "no", combate: 56},
+    { nombre: "Rocky Marciano", edad: 46, estatura: "179cm", peso: "85kg", division: ["pesos pesado"], campeon: [ "WBO" ], activo: "no", combate: 49 }, 
+    { nombre: "Terence Crawford", edad: 34, estatura: "173cm", peso: "75kg", division: ["peso welter", "peso superligero", "peso ligero"], campeon: [ "WBC", "WBO","IBF", "WBA" ], activo: "si", combate: 37 }, 
+    { nombre: "Roberto Duran", edad: 70, estatura: "170cm", peso: "70kg", division: ["peso ligero"], campeon: [ "FIB" ], activo: "no", combate: 119 }, 
+    { nombre: "Anthony Joshua", edad: 32, estatura: "198cm", peso: "109kg", division: ["peso pesado", "peso superpesado"], campeon: [ "IBF", "WBA", "IBO", "WBO" ], activo: "si", combate: 26 }, 
+    { nombre: "Vasyl Lomachenko", edad: 33 , estatura: "170cm", peso: "61kg", division: ["peso ligero, peso pluma, peso superpluma"], campeon: [ "WBA", "WBC", "WBO" ], activo: "si", combate: 16 },      
+    ] )
